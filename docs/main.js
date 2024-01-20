c115ee828374d590879e460b8bf969585c43f617
SinethMendis <mendissineth03@gmail.com>
2024-01-20 11:57:40 +0530

    Committing using the gitbash

--- docs/main.js
+++ docs/main.js
@@ -1,48 +1,123 @@
+// let num1;
+// let num2;
+// let operator = prompt("Please enter the operator");
+
+// calculate(operator);
+
+// function calculate(operator) {
+
+//     switch (operator) {
+//         case "+":
+//             num1 = Number(prompt("Enter the augend"))
+//             num2 = Number(prompt("Enter the addend"))
+//             console.log(`${add(num1, num2)}`)
+//             break;
+
+//         case "-":
+//             num1 = Number(prompt("Enter the minuend"))
+//             num2 = Number(prompt("Enter the subtrahend"))
+//             console.log(subtract(num1, num2))
+//             break;
+
+//         case "*":
+//             num1 = Number(prompt("Enter the multiplicand"))
+//             num2 = Number(prompt("Enter the multiplier"))
+//             console.log(multiply(num1, num2))
+//             break;
+
+//         case "/":
+//             num1 = Number(prompt("Enter the dividend"))
+//             num2 = Number(prompt("Enter the divisor"))
+//             if (num2 === 0) {
+//                 prompt("Can't divide by zero!")
+//             }
+//             else {
+//                 console.log(divide(num1, num2))
+//             }
+//             break;
+
+//         default:
+//             console.log("Invalid operator")
+//             break;
+
+//     }
+
+// }
+
 let num1;
-let num2;
-let operator = prompt("Please enter the operator");
+let myArrayWithElements = [1, 2, 3];
 
-calculate(operator);
+const oneButton = document.getElementById("one-button");
+const twoButton = document.getElementById("two-button");
+const threeButton = document.getElementById("three-button");
+const fourButton = document.getElementById("four-button");
+const fiveButton = document.getElementById("five-button");
+const sixButton = document.getElementById("six-button");
+const sevenButton = document.getElementById("seven-button");
+const eightButton = document.getElementById("eight-button");
+const nineButton = document.getElementById("nine-button");
 
-function calculate() {
+const screen = document.querySelector(".screen");
 
-    switch (operator) {
-        case "+":
-            num1 = Number(prompt("Enter the augend"))
-            num2 = Number(prompt("Enter the addend"))
-            alert(`${add(num1, num2)}`)
-            break;
+const fontSize = 45;
+const aspectRatio = 0.35;
+const averageCharacterWidth = fontSize * aspectRatio;
 
-        case "-":
-            num1 = Number(prompt("Enter the minuend"))
-            num2 = Number(prompt("Enter the subtrahend"))
-            alert(subtract(num1, num2))
-            break;
-            
-        case "*":
-            num1 = Number(prompt("Enter the multiplicand"))
-            num2 = Number(prompt("Enter the multiplier"))
-            alert(multiply(num1, num2))
-            break;
 
-        case "/":
-            num1 = Number(prompt("Enter the dividend"))
-            num2 = Number(prompt("Enter the divisor"))
-            if (num2 === 0) {
-                prompt("Can't divide by zero!")
-            }
-            else {
-                alert(divide(num1, num2))
-            }
-            break;
+const maxContentLength = Math.floor(screen.clientWidth / averageCharacterWidth);
 
-        default:
-            console.log("Invalid operator")
-            break;
-
+function appendToScreen(content) {
+    if (screen.textContent.length < maxContentLength) {
+        screen.textContent += content;
+    } else {
+        console.log("Content limit reached");
     }
-
 }
+~
+
+
+oneButton.addEventListener("click", function () {
+    appendToScreen("\uD835\uDFCF");
+    num1 = 1;
+});
+
+twoButton.addEventListener("click", function () {
+    appendToScreen("\uD835\uDFD0");
+    num1 = 2;
+});
+
+threeButton.addEventListener("click", function () {
+    appendToScreen("\uD835\uDFD1");
+});
+
+fourButton.addEventListener("click", function () {
+    appendToScreen("\uD835\uDFD2");
+});
+
+fiveButton.addEventListener("click", function () {
+    appendToScreen("\uD835\uDFD3");
+});
+
+sixButton.addEventListener("click", function () {
+    appendToScreen("\uD835\uDFD4");
+});
+
+sevenButton.addEventListener("click", function () {
+    appendToScreen("\uD835\uDFD5");
+});
+
+eightButton.addEventListener("click", function () {
+    appendToScreen("\uD835\uDFD6");
+});
+
+nineButton.addEventListener("click", function () {
+    appendToScreen("\uD835\uDFD7");
+});
+
+
+
+
+
 
 function add(num1, num2) {
     return num1 + num2;
@@ -57,3 +132,4 @@ function subtract(num1, num2) {
 
     return num1 / num2;
 }
+
